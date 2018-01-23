da04be8c979850a6c69a89eac7f8ee9fa309b877
smalois <smalois15@gmail.com>
2018-01-22 21:13:58 -0500

    properly generate board

--- src/main.rs
+++ src/main.rs
@@ -32,15 +32,17 @@ static HEIGHT: i32 = 8;
 static NUM_MINES: usize = 10;
 
 fn init_board(board : &mut Vec<Cell>) {
-    for i in 0..(WIDTH * HEIGHT) {
-        board.push( Cell { x_coord: i % WIDTH, y_coord: i % HEIGHT, is_mine: false, adjacent_mines: 0} );
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            board.push( Cell { x_coord: x, y_coord: y, is_mine: false, adjacent_mines: 0} );
+        }
     }
 }
 
 fn init_mines(board : &mut Vec<Cell>) {
     // Generate a vector of mines (tuples) without repeats.
     let mut mines : Vec<(i32, i32)> = Vec::new();
-    while mines.len() <= NUM_MINES {
+    while mines.len() < NUM_MINES {
         let x_loc : i32 = rand::thread_rng().gen_range(0, WIDTH);
         let y_loc : i32 = rand::thread_rng().gen_range(0, HEIGHT);
         if !mines.contains( &(x_loc, y_loc) ) {
@@ -50,27 +52,42 @@ fn init_mines(board : &mut Vec<Cell>) {
 
     // Set the mine values in the board
     for mine in mines {
-        board[(mine.0 * mine.1) as usize].set_mine();
+        board[(mine.0 + (WIDTH * mine.1)) as usize].set_mine();
     }
 }
 
 fn init_cell_values(board : &mut Vec<Cell>) {
-    for cell in board.iter() {
-        if cell.is_mine {
-            let nine_grid_x_bounds = (cmp::max(cell.x_coord - 1, 0), cmp::min(cell.x_coord + 1, WIDTH));
-            let nine_grid_y_bounds = (cmp::max(cell.y_coord - 1, 0), cmp::min(cell.y_coord + 1, HEIGHT));
-            for y in nine_grid_y_bounds.0..nine_grid_y_bounds.1 {
-                for x in nine_grid_x_bounds.0..nine_grid_x_bounds.1 {
-                    board[(x * y) as usize].add_one();
-              }
+    for row in 0..HEIGHT {
+        for col in 0..WIDTH {
+            if board[(col + (WIDTH * row)) as usize].is_mine {
+                let nine_grid_y_bounds = (cmp::max(row - 1, 0), cmp::min(row + 1, HEIGHT - 1));
+                let nine_grid_x_bounds = (cmp::max(col - 1, 0), cmp::min(col + 1, WIDTH - 1));
+                for y in nine_grid_y_bounds.0..nine_grid_y_bounds.1 + 1 {
+                    for x in nine_grid_x_bounds.0..nine_grid_x_bounds.1 + 1 {
+                        board[(x + y * WIDTH) as usize].add_one();
+                    }
+                }
             }
+        }
+    }
+}
+
+fn print_mines(board : &mut Vec<Cell>) {
+    for i in board {
+        if i.is_mine {
+            print!("1 ");
+        } else {
+            print!("0 ");
+        }
+        if i.x_coord == WIDTH - 1 {
+            print!("\n");
         }
     }
 }
 
 fn print_board(board : &mut Vec<Cell>) {
     for i in board {
-        print!("{} ", i.is_mine);
+        print!("{} ", i.adjacent_mines);
         if i.x_coord == WIDTH - 1 {
             print!("\n");
         }
@@ -82,5 +99,6 @@ fn main() {
     let mut board : Vec<Cell> = Vec::new();
     init_board(&mut board);
     init_mines(&mut board);
+    init_cell_values(&mut board);
     print_board(&mut board);
 }
