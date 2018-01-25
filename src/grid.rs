f6fb1e42b32d490d80744b0e0d5e48e54293b82b
smalois <smalois15@gmail.com>
2018-01-24 22:43:44 -0500

    added params to grid

--- src/grid.rs
+++ src/grid.rs
@@ -4,10 +4,6 @@ use self::rand::Rng;
 use std::cmp;
 
 
-static WIDTH: i32 = 10;
-static HEIGHT: i32 = 8;
-static NUM_MINES: usize = 10;
-
 mod cell {
     pub struct Cell {
         is_mine: bool,
@@ -41,13 +37,19 @@ mod cell {
 }
 
 pub struct Board {
+    width: i32,
+    height: i32,
+    num_mines: usize,
     cells : Vec<cell::Cell>,
 }
 
 impl Board {
     // TODO receive board dimensions and number of mines
-   pub fn new() -> Board {
+   pub fn new(width: i32, height: i32, num_mines: usize) -> Board {
         let mut b : Board = Board {
+            width: width,
+            height: height,
+            num_mines: num_mines,
             cells: Vec::new(),
         };
         b.init_board();
@@ -57,8 +59,8 @@ impl Board {
     }
 
     fn init_board(&mut self) {
-        for _ in 0..HEIGHT {
-            for _ in 0..WIDTH {
+        for _ in 0..self.height {
+            for _ in 0..self.width {
                 self.cells.push( cell::Cell::new() ); 
             }
         }
@@ -67,9 +69,9 @@ impl Board {
     fn init_mines(&mut self) {
         // Generate a vector of mines (tuples) without repeats.
         let mut mines : Vec<(i32, i32)> = Vec::new();
-        while mines.len() < NUM_MINES {
-            let x_loc : i32 = rand::thread_rng().gen_range(0, WIDTH);
-            let y_loc : i32 = rand::thread_rng().gen_range(0, HEIGHT);
+        while mines.len() < self.num_mines {
+            let x_loc : i32 = rand::thread_rng().gen_range(0, self.width);
+            let y_loc : i32 = rand::thread_rng().gen_range(0, self.height);
             if !mines.contains( &(x_loc, y_loc) ) {
                 mines.push( (x_loc, y_loc) );
             }
@@ -77,21 +79,21 @@ impl Board {
 
         // Set the mine values in the board
         for mine in mines {
-            self.cells[(mine.0 + (WIDTH * mine.1)) as usize].set_mine();
+            self.cells[(mine.0 + (self.width * mine.1)) as usize].set_mine();
         }
     }
 
     fn init_cell_values(&mut self) {
         // Iterate through entire grid
-        for row in 0..HEIGHT {
-            for col in 0..WIDTH {
-                if self.cells[(col + (WIDTH * row)) as usize].is_mine() {
-                    let surrounding_grid_y_bounds = (cmp::max(row - 1, 0), cmp::min(row + 1, HEIGHT - 1));
-                    let surrounding_grid_x_bounds = (cmp::max(col - 1, 0), cmp::min(col + 1, WIDTH - 1));
+        for row in 0..self.height {
+            for col in 0..self.width {
+                if self.cells[(col + (self.width * row)) as usize].is_mine() {
+                    let surrounding_grid_y_bounds = (cmp::max(row - 1, 0), cmp::min(row + 1, self.height - 1));
+                    let surrounding_grid_x_bounds = (cmp::max(col - 1, 0), cmp::min(col + 1, self.width - 1));
                     // Iterate through the 3x3 cell block surrounding the current cell
                     for y in surrounding_grid_y_bounds.0..surrounding_grid_y_bounds.1 + 1 {
                         for x in surrounding_grid_x_bounds.0..surrounding_grid_x_bounds.1 + 1 {
-                            self.cells[(x + y * WIDTH) as usize].add_one();
+                            self.cells[(x + y * self.width) as usize].add_one();
                         }
                     }
                 }
@@ -106,7 +108,7 @@ impl Board {
             } else {
                 print!("0 ");
             }
-            if (i + 1) % WIDTH as usize == 0 {
+            if (i + 1) % self.width as usize == 0 {
                 print!("\n");
             }
         }
@@ -115,7 +117,7 @@ impl Board {
     pub fn print_board(&self) {
         for (i, cell) in self.cells.iter().enumerate() {
             print!("{} ", cell.num_adjacent_mines());
-            if (i + 1) % WIDTH as usize == 0 {
+            if (i + 1) % self.width as usize == 0 {
                 print!("\n");
             }
         }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 mod grid;
 
+static WIDTH: i32 = 10;
+static HEIGHT: i32 = 8;
+static NUM_MINES: usize = 10;
+
 fn main() {
     // vec2d exists
-    let board : grid::Board =  grid::Board::new();
+    let board : grid::Board =  grid::Board::new(WIDTH, HEIGHT, NUM_MINES);
     board.print_board();
     board.print_mines();
 }
